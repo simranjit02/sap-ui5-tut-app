77099db3285cd75bf3177569609b11585780edfb
simranjit02 <simranjitsingh58648@gmail.com>
2025-04-09 00:17:11 +0300

    integrate json model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,21 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  (Controller, MessageToast) => {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  (Controller, MessageToast, JSONModel) => {
     "use strict";
     return Controller.extend("ui5.walkthrow.controller.App", {
+      onInit() {
+        const oData = {
+          recipient: {
+            name: "World",
+          },
+        };
+        const oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+      },
       sayHello() {
         MessageToast.show("Hello from App.controller.js");
       },
