f19e1745dad21660f0f8b95351c1015d63d47484
simranjit02 <simranjitsingh58648@gmail.com>
2025-04-10 12:58:37 +0300

    Step 9: Component Configuration

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,26 +5,9 @@ sap.ui.define(
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel",
   ],
-  (Controller, MessageToast, JSONModel, ResourceModel) => {
+  (Controller, MessageToast) => {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrow.controller.App", {
-      onInit() {
-        const oData = {
-          recipient: {
-            name: "World",
-          },
-        };
-        const oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
-        const i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocale: "",
-        });
-
-        this.getView().setModel(i18nModel, "i18n");
-      },
-
       sayHello() {
         const oBundle = this.getView().getModel("i18n").getResourceBundle();
         const sRecipient = this.getView()
--- webapp/index.js
+++ webapp/index.js
@@ -1,15 +1,26 @@
 sap.ui.define(
   [
     // "sap/m/Text"
-    "sap/ui/core/mvc/XMLView",
+    // "sap/ui/core/mvc/XMLView",
+    "sap/ui/core/ComponentContainer",
   ],
-  (XMLView) => {
+  (
+    // XMLView
+    ComponentContainer
+  ) => {
     "use strict";
     // new Text({ text: "Hello sap UI5..." }).placeAt("contentH1");
-    XMLView.create({
-      viewName: "sap.ui.demo.walkthrough.view.App",
-    }).then((oView) => {
-      oView.placeAt("contentH1");
-    });
+    // XMLView.create({
+    //   viewName: "sap.ui.demo.walkthrough.view.App",
+    // }).then((oView) => {
+    //   oView.placeAt("contentH1");
+    // });
+    new ComponentContainer({
+      name: "sap.ui.demo.walkthrough",
+      settings: {
+        id: "walkthrough",
+      },
+      async: true,
+    }).placeAt("contentH1");
   }
 );
