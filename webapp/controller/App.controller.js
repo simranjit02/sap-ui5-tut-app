c03697038b42d08078c5feb852b13b9859b183fe
simranjit02 <simranjitsingh58648@gmail.com>
2025-04-10 11:57:06 +0300

    integrate the translation-i18n model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,10 +3,11 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  (Controller, MessageToast, JSONModel) => {
+  (Controller, MessageToast, JSONModel, ResourceModel) => {
     "use strict";
-    return Controller.extend("ui5.walkthrow.controller.App", {
+    return Controller.extend("sap.ui.demo.walkthrow.controller.App", {
       onInit() {
         const oData = {
           recipient: {
@@ -15,9 +16,22 @@ sap.ui.define(
         };
         const oModel = new JSONModel(oData);
         this.getView().setModel(oModel);
+        const i18nModel = new ResourceModel({
+          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+          supportedLocales: [""],
+          fallbackLocale: "",
+        });
+
+        this.getView().setModel(i18nModel, "i18n");
       },
+
       sayHello() {
-        MessageToast.show("Hello from App.controller.js");
+        const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        const sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        MessageToast.show(sMsg);
       },
     });
   }
