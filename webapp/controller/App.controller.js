134154a73ab75d93705eec086bace7ef48041f6e
simranjit02 <simranjitsingh58648@gmail.com>
2025-04-14 13:36:07 +0300

    integrate nested view

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,4 @@
-sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  (Controller, MessageToast) => {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrow.controller.App", {
-      onShowHello() {
-        const oBundle = this.getView().getModel("i18n").getResourceBundle();
-        const sRecipient = this.getView()
-          .getModel()
-          .getProperty("/recipient/name");
-        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-        MessageToast.show(sMsg);
-      },
-    });
-  }
-);
+sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+  "use strict";
+  return Controller.extend("sap.ui.demo.walkthrow.controller.App", {});
+});
---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
+    "use strict";
+
+    return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+      onShowHello() {
+        const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        const sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        MessageToast.show(sMsg);
+      },
+    });
+  }
+);
